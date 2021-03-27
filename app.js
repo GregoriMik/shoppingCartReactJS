b62ddecf46e21fb5d2b2fd2b2c79ff956d3de00b
Gregori Mikina <gregorimikina@gmail.com>
2021-03-27 21:43:49 +0100

    Create a subtract lock if it is zero

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 class App extends React.Component{
   state = {
-    
+    availableProducts: 7,
+    shoppingCart: 0,
   }
   render() {
     return(
       <>
-        <button disabled>-</button>
-        <span> 0 </span>
+        <button disabled={this.state.shoppingCart===0? true : false}>-</button>
+        <span> {this.state.shoppingCart} </span>
         <button>+</button>
       </>
     )
